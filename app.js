db2335ddab1b88aca45a89d5540135c1c7339adc
sahrul programing <sahrul.saefullah91@gmail.com>
2021-03-01 16:27:35 +0700

    perbaikan if visi himsi

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ client.on('message', msg => {
     const pesan = msg.body.toLowerCase()
     console.log(pesan)
     if (pesan.includes('hallo himsi') || pesan.includes('hello himsi') || pesan.includes('helo himsi') || pesan.includes('halo himsi') ) {
-        msg.reply('hallo Juga '+pengirim);
-    }else if (pesan.includes('visi himsi') == true) {
+        msg.reply('hello Juga '+pengirim);
+    }else if (pesan.includes('visi himsi') || pesan.includes('visi dari himsi')) {
         msg.reply('Menjadikan HIMSI sebagai Himpunan yang kreatif, kompetitif, bertanggung jawab dan Berwawasan Global pada tahun 2024')
     }else if (pesan.includes('apa itu himsi')) {
         msg.reply('HIMSI adalah singkatan dari (Himpunan Mahasiswa Sistem Informasi) berdiri pada tahun 2018 \n \n yang semulanya bernama HIMMI (Himpunan Mahasiswa Management Informatika)')
